8d3ea7492df0d7f0c5019e64d1328fa896f33c11
mustafa <mustafasen@tuta.io>
2022-05-05 21:17:11 +0300

    added comments

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
+//listData recieves mvPurchaseOrders array in parsed json object
 function createList(listData) {
 
     const ul = document.createElement("ul");
-    
+    //Creates list elements for each element in mvPurchaseOrders array
     for (let i = 0; i < listData.length; i++) {
         
         const element = listData[i];
@@ -16,28 +17,29 @@ function createList(listData) {
 function createTable(tableData, reqAttr) {
 
     const table = document.createElement('table')
-
+    //Table data contains PurchaseOrderDetails of specific order
     for (let i = -1; i < tableData.length; i++) {
-
+        //Creating table row for each each element in tabledata
         const tr = document.createElement('tr');
-        
+        //Traversing each element in requested attributes
         reqAttr.forEach(element => {
-
+            //If i is -1 create header with given attribute names (in reqAttr)
             if(i == -1) {
                 const th = document.createElement('th');
                 th.innerText = element;
                 tr.appendChild(th);
             } else {
+                //Else get the value of specific keys from reqAttr
                 const td = document.createElement('td');
                 td.innerText = tableData[i][element];
                 tr.appendChild(td);
             }
 
         });
-
+        //Append the row filled with data
         table.appendChild(tr);
     }
-    
+    //Return table
     return table;
 }
 
